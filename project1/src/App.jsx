bd9dcd941b4d844377ba965f95359fc7b2ca6b2a
uysolanki <uysolanki@gmail.com>
2026-01-02 15:55:49 +0530

    produts display

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from './components/Navbar'
 import Navbar1 from './components/Navbar1'
 import Navbar2 from './components/Navbar2'
+import Test from './components/Test'
 
 const App = () => {
   return (
     <div>
-    <Navbar2/>
+    <Test/>
     </div>
   )
 }
